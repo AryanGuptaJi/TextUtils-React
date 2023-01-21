a8388c93ab72b8589b96b029c80ba5469daa16ad
Aryan Gupta <aryang23936@gmail.com>
2023-01-22 02:23:10 +0530

    Updates

--- module1.mjs
+++
@@ -1,7 +0,0 @@
-// import ui from './module2.mjs'
-// console.log(ui);
-
-import ui, {b,c} from './module2.mjs'
-console.log(ui);
-console.log(b);
-console.log(c);
--- module2.mjs
+++
@@ -1,7 +0,0 @@
-const a = "Aryan";
-const b = "Gupta";
-const c = "Akash";
-
-export default a;
-export {b};
-export {c};
